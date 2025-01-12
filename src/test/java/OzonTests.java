6f9e6aafec7a16dc954c01796013701d92e714ef
Anastasia Lyuleva <nesti-1998@mail.ru>
2025-01-12 18:20:59 +0300

    change test

--- src/test/java/OzonTests.java
+++ src/test/java/OzonTests.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,7 +14,8 @@ public class OzonTests extends TestBase{
 })
 @ParameterizedTest (name = "Проверка поиска по слову {0}")
 void checkSearchOzon(String clothes){
-    $("name = text").setValue(clothes);
+    $("[name='text']").setValue(clothes);
+    $("[aria-label='Поиск']").click();
     $("#paginator").shouldHave(text(clothes));
 }
 }
