442a656e855fac5885828512ff873524170e78c4
Defoltini <nesti-1998@mail.ru>
2025-01-12 20:31:10 +0300

    Added test with MethodSource

---
+++ src/test/data/Currency.java
@@ -0,0 +1,9 @@
+public enum Currency {
+    KGS("сом"),
+    KZT("тг.");
+    public final String description;
+    Currency(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/test/java/WBTests.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WBTests extends TestBase{
+@Tag("High")
+@Tag("Smoke")
+@CsvSource({
+        "брюки",
+        "кофта"
+})
+@ParameterizedTest (name = "Проверка поиска по слову {0}")
+void checkSearchWB(String clothes){
+    open("https://www.wildberries.ru/");
+    $("#searchInput").setValue(clothes);
+    $("#applySearchBtn").click();
+    $("#mainContainer").shouldHave(text(clothes));
+}
+@EnumSource(Currency.class)
+    @Tag("High")
+    @Tag("Smoke")
+    @ParameterizedTest
+    void checkСurrencyOnStartPage(Currency currency){
+    open("https://www.wildberries.ru/");
+
+    $("[data-wba-header-name='Country']").click();
+    $$(".country__item.j-b-change-settings").find(text(currency.name())).click();
+    $("#body-layout").shouldHave(text(currency.description));
+    }
+    static Stream<Arguments> checkСurrencyOnStartPageWithMethodSource(){
+        return Stream.of(
+                Arguments.of(
+                        Currency.KGS,
+                        List.of("сом")
+                ),
+                Arguments.of(
+                        Currency.KZT,
+                        List.of("тг.")));
+    }
+    @MethodSource
+    @ParameterizedTest
+    void checkСurrencyOnStartPageWithMethodSource(Currency currency, List<String> expectedCurrency){
+        open("https://www.wildberries.ru/");
+
+    $("[data-wba-header-name='Country']").click();
+    $$(".country__item.j-b-change-settings").find(text(currency.name())).click();
+    $("#body-layout").shouldHave(text(expectedCurrency));
+    }
+
+}
